3217dea75fd461ee1176be4b59641224dc35f62f
andyliuliming <andyliuliming@e1cf65d8-7a74-11de-9755-51fe06bdd687>
2011-09-30 12:42:30 +0000

    comment out in case of the permission issue.

--- tracking/Tracking/CommonResource/TrackingLog.cs
+++ tracking/Tracking/CommonResource/TrackingLog.cs
@@ -10,13 +10,13 @@ namespace CommonResource
     {
         public static void Log(string str)
         {
-            EventLog log = new EventLog("MyEvent");
-            //  首先应判断日志来源是否存在，一个日志来源只能同时与一个事件绑定s
-            if (!EventLog.SourceExists("New Application"))
-                EventLog.CreateEventSource("New Application", "MyEvent");
-            log.Source = "New Application";
-            log.WriteEntry("Error:" + str, EventLogEntryType.Information);
-            log.Close();
+            //EventLog log = new EventLog("MyEvent");
+            ////  首先应判断日志来源是否存在，一个日志来源只能同时与一个事件绑定s
+            //if (!EventLog.SourceExists("New Application"))
+            //    EventLog.CreateEventSource("New Application", "MyEvent");
+            //log.Source = "New Application";
+            //log.WriteEntry("Error:" + str, EventLogEntryType.Information);
+            //log.Close();
         }
     }
 }
